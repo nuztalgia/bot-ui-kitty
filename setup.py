8d656c60b89f4d6ae1fa27e4bf5c69709df31a0a
Nuz / Lovegood <nuztalgia@gmail.com>
2022-11-18 21:08:28 -0800

    Update project setup files

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
 setup(
+    url="https://github.com/nuztalgia/bot-ui-kitty",
+    project_urls={
+        "Issue Tracker": "https://github.com/nuztalgia/bot-ui-kitty/issues",
+        "Source Code": "https://github.com/nuztalgia/bot-ui-kitty/tree/main/uikitty",
+    },
     install_requires=[
         "py-cord ==2.2.2",
     ],
-    url="https://github.com/nuztalgia/bot-ui-kitty",
+    use_scm_version={
+        "local_scheme": lambda version: str(version.tag),
+        "version_scheme": lambda _: "",
+    },
 )
